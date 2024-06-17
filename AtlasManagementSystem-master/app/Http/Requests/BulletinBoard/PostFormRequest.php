7601e863f636209781c7dc97b02eedaf9a3a585f
伊藤大地 <taichi37taichi@gmail.com>
2024-06-17 21:16:50 +0900

    ファイルの修正をいたしました。 ・バリデーション条件の修正 ・Seederファイルの修正

--- AtlasManagementSystem-master/app/Http/Requests/BulletinBoard/PostFormRequest.php
+++ AtlasManagementSystem-master/app/Http/Requests/BulletinBoard/PostFormRequest.php
@@ -24,17 +24,19 @@ class PostFormRequest extends FormRequest
     public function rules()
     {
         return [
-            'post_title' => 'min:4|max:50',
-            'post_body' => 'min:10|max:500',
+            'post_title' => ['required', 'max:100', 'string'],
+            'post_body' => ['required', 'max:5000','string'],
         ];
     }
 
     public function messages(){
         return [
-            'post_title.min' => 'タイトルは4文字以上入力してください。',
-            'post_title.max' => 'タイトルは50文字以内で入力してください。',
-            'post_body.min' => '内容は10文字以上入力してください。',
-            'post_body.max' => '最大文字数は500文字です。',
+            'post_title.required' => 'タイトルは必須です',
+            'post_title.max' => 'タイトルは100文字以内で入力してください',
+            'post_title.string' => 'タイトルは文字列で入力してください',
+            'post_body.required' => '投稿内容は必須です',
+            'post_body.max' => '投稿内容は5000文字以内で入力してください',
+            'post_body.string' => '投稿内容は文字列で入力してください',
         ];
     }
 }
